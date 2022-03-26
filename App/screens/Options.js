1faa9df98996551102d93210cfe5d33e1684199f
Pratima <pratima.waghmare@logiquad.com>
2022-03-26 23:37:02 +0530

    Worked on custom components and linking

---
+++ App/screens/Options.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, StatusBar, Linking, Alert } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+
+import colors from '../constants/colors';
+import { RowItem, RowSeperator } from '../components/RowItem';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight,
+
+    },
+})
+
+const openUrl = (url) => {
+    return (
+        Linking.openURL(url).catch(() => {
+            Alert.alert('Sorry, something went wrong.', 'Please try again.');
+        })
+    )
+}
+export default () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <RowItem
+                rightIcon={<Entypo name='chevron-right' size={20} color={colors.blue} />}
+                text="Themes"
+                onPress={() => alert('todo!')}
+            />
+
+            <RowSeperator />
+
+            <RowItem
+                rightIcon={<Entypo name='export' size={20} color={colors.blue} />}
+                text="React Native Basics"
+                onPress={() => openUrl(
+                    'https://learn.reactnativeschool.com/p/react-native-basics-build-a-currency-converter'
+                )}
+            />
+
+            <RowSeperator />
+
+            <RowItem
+                rightIcon={<Entypo name='export' size={20} color={colors.blue} />}
+                text="React Native by Example"
+                onPress={() => openUrl('httssfps://reactnativebyexample.com')}
+            />
+        </SafeAreaView>
+    );
+};
