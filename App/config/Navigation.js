1a132b60e01e7c5874a522a3707cef0ac20b38c7
Pratima <pratimawaghmare44@gmail.com>
2022-04-03 19:38:02 +0530

    Changed the currency list rendering mode to modal

--- App/config/Navigation.js
+++ App/config/Navigation.js
@@ -1,37 +1,53 @@
 import React from "react"
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
+import { TouchableOpacity } from "react-native"
+import { Entypo } from '@expo/vector-icons'
 
 import Home from "../screens/Home"
 import Options from "../screens/Options"
 import CurrencyList from "../screens/CurrencyList"
+import colors from "../constants/colors"
 
 const MainStack = createStackNavigator()
 
-const MainStackScreen = () => {
-    return (
-        <MainStack.Navigator
-        //headerMode="none"
-        //initialRouteName="CurrencyList"
-        >
-            <MainStack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-            <MainStack.Screen name="Options" component={Options} />
-            <MainStack.Screen
-                name="CurrencyList"
-                component={CurrencyList}
-                options={({ route }) => ({
-                    title: route.params && route.params.title
-                })}
-            />
-        </MainStack.Navigator>
-    );
-}
+const MainStackScreen = () => (
+    <MainStack.Navigator
+    //headerMode="none"
+    //initialRouteName="CurrencyList"
+    >
+        <MainStack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+        <MainStack.Screen name="Options" component={Options} />
+    </MainStack.Navigator>
+)
+
+const ModalStack = createStackNavigator()
+const ModalStackScreen = () => (
+    <ModalStack.Navigator screenOptions={{ presentation: "modal" }}>
+        <ModalStack.Screen name="Main" component={MainStackScreen} options={{ headerShown: false }} />
+        <ModalStack.Screen
+            name="CurrencyList"
+            component={CurrencyList}
+            options={({ navigation, route }) => ({
+                title: route.params && route.params.title,
+                headerLeft: null,
+                headerRight: () => (
+                    <TouchableOpacity
+                        onPress={() => navigation.pop()}
+                        style={{ paddingHorizontal: 10 }}>
+                        <Entypo name="cross" size={30} color={colors.blue} />
+                    </TouchableOpacity>
+                ),
+            })}
+        />
+    </ModalStack.Navigator>
+)
 
 export default () => {
 
     return (
         <NavigationContainer>
-            <MainStackScreen />
+            <ModalStackScreen />
         </NavigationContainer>
     );
 }
