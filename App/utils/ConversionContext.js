d1996ad9aeb43baccdb844a9c11f08246f3ff264
Pratima <pratimawaghmare44@gmail.com>
2022-04-07 10:41:24 +0530

    calling the api in the conversioncontext to load the rates at app launch

--- App/utils/ConversionContext.js
+++ App/utils/ConversionContext.js
@@ -1,11 +1,37 @@
-import React, { useState, createContext } from 'react'
+import React, { useState, createContext, useEffect } from 'react'
+import { Alert } from 'react-native';
+import { api } from './api';
 
 export const ConversionContext = createContext();
 
-export const ConversionContextProvider = ({ children }) => {
-    const [baseCurrency, setBaseCurrency] = useState('USD')
-    const [quoteCurrency, setQuoteCurrency] = useState('GBP')
+const DEFAULT_BASE_CURRENCY = "USD"
+const DEFAULT_QUOTE_CURRENCY = "GBP"
 
+export const ConversionContextProvider = ({ children }) => {
+    const [baseCurrency, _setBaseCurrency] = useState(DEFAULT_BASE_CURRENCY)
+    const [quoteCurrency, setQuoteCurrency] = useState(DEFAULT_QUOTE_CURRENCY)
+    const [date, setDate] = useState()
+    const [rates, setRates] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+
+    const setBaseCurrency = (currency) => {
+        setIsLoading(true)
+
+        return api(`/latest?base=${currency}`)
+            .then((response) => {
+                console.log("response", response)
+                _setBaseCurrency(currency)
+                setDate(response.date)
+                setRates(response.rates)
+            })
+            .catch((error) => {
+                console.log("error", error)
+                Alert.alert('Sorry, something went wrong.', 'Please try again.')
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
+    }
     const swapCurrencies = () => {
         setBaseCurrency(quoteCurrency)
         setQuoteCurrency(baseCurrency)
@@ -16,8 +42,15 @@ export const ConversionContextProvider = ({ children }) => {
         quoteCurrency,
         swapCurrencies,
         setBaseCurrency,
-        setQuoteCurrency
+        setQuoteCurrency,
+        date,
+        rates,
+        isLoading
     }
+
+    useEffect(() => {
+        setBaseCurrency(DEFAULT_BASE_CURRENCY)
+    }, [])
 
     return (
         <ConversionContext.Provider value={contextValue}>
